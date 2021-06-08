13d0a704fee875bb847af0e32463471eb780d135
MashalAman12 <ramzanjeni133@gmail.com>
2021-06-08 09:27:58 +0500

    Added logic to fatch data for university names and comparisons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const sql = require("mssql");
+const { query } = require("express");
 
 // configuring database
 var config = {
@@ -24,7 +25,8 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/searchUniversity", function (req, res) {
+//get all the information of universities
+app.post("/searchUniversity", (req, res) => {
     console.log("inside searchUniversity function")
     const getData = {
         universityType: req.body.universityType,
@@ -32,16 +34,21 @@ app.get("/searchUniversity", function (req, res) {
         program: req.body.program,
     }
 
+    var name = req.body.universityName;
+    console.log(name);
+
     console.log(JSON.stringify(getData));
 
     sql.connect(config).then(pool => {
         // Query
         var request = new sql.Request();
-        request.query('select * from University', function (err, recordset) {
+        var sqlQuery = "SELECT * FROM searchUniversity where name='" + name + "'";
+        request.query(sqlQuery, function (err, recordset) {
             if (err) {
                 console.log("Query error is: " + err);
             } else {
                 res.send(recordset);
+                console.log(recordset);
                 console.log('data sent');
             }
         });
@@ -49,6 +56,49 @@ app.get("/searchUniversity", function (req, res) {
 
 });
 
+//get the universities names
+app.get("/getSearchedData", (req, res) => {
+
+    console.log("Inside get method");
+    sql.connect(config).then(pool => {
+
+        var request = new sql.Request();
+        var sqlQuery = "SELECT * FROM searchUniversity";
+        request.query(sqlQuery, function (err, data) {
+
+            if (err) {
+                console.log(err);
+            } else {
+                
+                var uniNames = '';
+                var len = data.recordsets.length;
+                for (var i = 0; i < len; i++) {
+                    for (var j = 0;j < 7;j++) {
+                        uniNames += '<option>' + data.recordsets[i][j].name + '</option>';
+                    }
+                }
+                res.send(uniNames);
+            }
+        })
+    })
+})
+
+//get university comparison data
+app.post("/getComparison", (req, res) => {
+
+    sql.connect(config).then(pool => {
+        var request = new sql.Request();
+        var sqlQuery = "Select * from Comparison";
+        request.query(sqlQuery, (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("success");
+                res.send(data);
+            }
+        })
+    })
+})
 
 var server = app.listen(5000, function () {
 
